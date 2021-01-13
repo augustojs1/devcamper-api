c1993a58122e625a5abd0b725dadb74d14755b9e
Augusto <augustojsouza1@gmail.com>
2021-01-12 21:21:55 -0300

    Added logging middleware

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-//Loading env variables
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Loading env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Dev loggin middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
